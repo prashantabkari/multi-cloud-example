c3b50e428e556a696bb9ab8cac2b68877c231e80
prashantabkari <prashantabkari@gmail.com>
2021-01-22 05:39:07 +0000

    modified script

--- create.sh
+++ create.sh
@@ -1,10 +1,12 @@
 
-PYTHON_PATH="python/lib/python3.7/site-packages"
+PYTHON_PATH="lib/python3.7/site-packages"
 python3 -m venv gcpenv
+source gcpenv/bin/activate 
 pip install --upgrade google-cloud-vision
 
+deactivate
 mkdir -p gcp_layer/python
-cp gcpenv/$PYTHON_PATH/* gcp_layer/python/
+cp -r gcpenv/$PYTHON_PATH/* gcp_layer/python/.
 cd  gcp_layer
 zip -r9 ../gcp_layer_package.zip *
 cd ..
@@ -15,5 +17,5 @@ aws lambda publish-layer-version \
     --zip-file fileb://gcp_layer_package.zip \
     --compatible-runtimes python3.7
     
-GCP_LAYER_VERSION_ARN=`aws lambda get-layer-version --layer-name Google_cloud_Vision --query LayerVersionArn`
+GCP_LAYER_VERSION_ARN=`aws lambda get-layer-version --version-number=1 --layer-name Google_cloud_Vision --query LayerVersionArn`
 
